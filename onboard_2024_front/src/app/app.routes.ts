0d0a3763e77e6a78cb7d274a03ca93f3a6d74855
Alexis <alexis.lequeux1003@outlook.fr>
2024-10-04 17:43:34 +0200

    [DEV] First pages

--- onboard_2024_front/src/app/app.routes.ts
+++ onboard_2024_front/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'home', component: HomePageComponent },
+    { path: '**', component: ErrorPageComponent },
+  ];
